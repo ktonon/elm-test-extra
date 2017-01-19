6c14ad6d18ad5a898c77c1706520a57ae8e73bb9
Kevin Tonon <kevin@betweenconcepts.com>
2017-01-18 20:23:11 -0500

    Added list membership expectation

---
+++ src/Expect/Extra.elm
@@ -0,0 +1,40 @@
+module Expect.Extra exposing (contain, member)
+
+{-| Extends Expect library with more `Expectation`s.
+
+@docs contain, member
+-}
+
+import Expect exposing (..)
+
+
+{-| Passes if value is a member of list.
+
+    member 1 [0, 1, 2]
+
+    -- Passes because 1 is a member of [0, 1, 2]
+-}
+member : a -> List a -> Expectation
+member value list =
+    if List.member value list then
+        pass
+    else
+        fail
+            ("Expected:\n  "
+                ++ (toString list)
+                ++ "\nto contain:\n  "
+                ++ (toString value)
+            )
+
+
+{-| Alias of `member`.
+
+Reads better with bdd style tests.
+
+    expect [0, 1, 2] to contain 1
+
+    -- Passes because [0, 1, 2] contains 1
+-}
+contain : a -> List a -> Expectation
+contain =
+    member
